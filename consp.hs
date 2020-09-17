409d66d46ebfd702d89560c4c799b1213445026f
Rousy <VALENTINA KARNAUHOVA>
2020-09-17 19:12:28 +0300

    cesar

--- consp.hs
+++ consp.hs
@@ -1,4 +1,19 @@
-main = putStrLn "consp"
+import Data.Char
+main = do
+    let s = cesar "test" 9
+    putStrLn s
+    putStrLn (dCesar s 9)
+    
 
-ceasar :: [a] -> a -> [a]
+--encode
+cesar :: [Char] -> Int -> [Char]
+cesar [] _ = []
+cesar (x:xs) n = f x : cesar xs n
+    where f = chr . mOd . (+n) . ord
+          mOd c = ((c - ord 'a') `mod` 26) + ord 'a'
 
+dCesar :: [Char] -> Int -> [Char]
+dCesar [] _ = []
+dCesar (x:xs) n = f x : dCesar xs n
+    where f = chr . mOd . (subtract n) . ord
+          mOd c = ((c - ord 'a') `mod` 26) + ord 'a'

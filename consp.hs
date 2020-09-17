0228d33d58c9d9dd4c85fb0f19010bb57208ae08
Rousy <VALENTINA KARNAUHOVA>
2020-09-17 19:16:00 +0300

    коменты для Виктора

--- consp.hs
+++ consp.hs
@@ -6,14 +6,14 @@ main = do
     
 
 --encode
-cesar :: [Char] -> Int -> [Char]
+cesar :: [Char] -> Int -> [Char] -- тип как в алгебре (принимаем массив чаров, затем число и возвращает массив чаров)
 cesar [] _ = [] -- для пустого массива пустой массив
 cesar (x:xs) n = f x : cesar xs n -- отковыриваем элемент и делаем с ним вещи
     where f = chr . mOd . (+n) . ord
           mOd c = ((c - ord 'a') `mod` 26) + ord 'a'
 
 --decode
-dCesar :: [Char] -> Int -> [Char]
+dCesar :: [Char] -> Int -> [Char] -- тип как в алгебре
 dCesar [] _ = [] -- для пустого массива пустой массив
 dCesar (x:xs) n = f x : dCesar xs n -- отковыриваем элемент и делаем с ним вещи
     where f = chr . mOd . (subtract n) . ord

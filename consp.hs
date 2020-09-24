65293250b92c7fbadec56fc1e56b82ac898f6be0
Your Name <you@example.com>
2020-09-24 15:19:49 +0300

    no time...

--- consp.hs
+++ consp.hs
@@ -4,17 +4,18 @@ import qualified Data.Map as M
 import System.IO
 
 main = do 
-    putStrLn "CESAR:"
-    let s = cesar 9 "test" 
-    putStrLn s 
-    putStrLn (dCesar 9 s ) 
+    s <- inspectFile (cesar 3) "test" "cout"
+    putStrLn ""
+    putStrLn . show . crackCesar $ s
+    s <- inspectFile (vigenere "lemon") "test" "cout"
+    putStrLn . show . kasiski $ s
 
-inspectFile :: (String -> String) -> String -> String -> IO() 
+inspectFile :: (String -> String) -> String -> String -> IO String
 inspectFile f ifn ofn 
-    | ofn == "cout" && ifn == "cin" = getContents >>= \s -> putStr (f s) 
-    | ofn == "cout" = readFile ifn >>= \s -> putStr (f s)
-    | ifn == "cin"  = getContents >>= \s -> writeFile ofn (f s)
-    | otherwise = readFile ifn >>= \s -> writeFile ofn (f s)
+    | ofn == "cout" && ifn == "cin" = getContents >>= \s -> putStr (f s) >> return (f s) 
+    | ofn == "cout" = readFile ifn >>= \s -> putStr (f s) >> return (f s)
+    | ifn == "cin"  = getContents >>= \s -> writeFile ofn (f s) >> return (f s)
+    | otherwise = readFile ifn >>= \s -> writeFile ofn (f s) >> return (f s)
 
 cesar :: Int -> [Char] -> [Char] 
 cesar _ []  = [] 
@@ -38,3 +39,23 @@ vigenere ns xs = zipWith f cs xs
     where f n = chr . mOd . (+n) . ord 
           cs  = cycle (map ((subtract (ord 'a') ) . ord) ns)
           mOd c = ((c - ord 'a') `mod` 26) + ord 'a'
+
+dVigenere :: [Char] -> [Char] -> [Char]
+dVigenere ns xs = zipWith f cs xs
+    where f n = chr . mOd . (subtract n) . ord 
+          cs  = cycle (map ((subtract (ord 'a') ) . ord) ns)
+          mOd c = ((c - ord 'a') `mod` 26) + ord 'a'
+
+kasiski :: [Char] -> [Double]
+kasiski s = helper s 1 []
+    where l = fromIntegral (length s) :: Double
+          ci s = 1/(l*l) * (li s) * (li s)
+          li s = fromIntegral $ sum (map (\a -> count a s) ['a'..'z'])
+          eq tol a b = tol > abs (a-b)
+          count x = length . filter (==x)
+          period [] t = []
+          period (s:ss) t = s : period (drop (t - 1) ss) t
+          helper s t buf | t > 26 = buf
+                         | otherwise = helper s (t + 1) (ci (period s t) : buf)
+                        -- | eq 5e-2 (ci (period s t)) 0.065 = t
+                        -- | otherwise = helper s (t + 1)
